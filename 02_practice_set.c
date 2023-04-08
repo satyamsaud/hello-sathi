676a3ee1cfe7078ebe59e7c5746b3094f816b0f8
satyamsaud <santoshsaud778625@gmail.com>
2023-04-08 16:21:50 +0545

    first commit

---
+++ 02_practice_set.c
@@ -0,0 +1,32 @@
+ //Question 1 & 2 <<<<
+ // #include<stdio.h>
+// int main(){
+// int a; int b=a;
+// int v = 3^3;
+// char dt = '2';
+// printf("%d", v);
+// float d = (3.0/8-2); 
+// printf("%f\n", d);
+// return 0;
+// }
+
+
+//  Q3 . write the program to determine wheather a number is divisible by 97 or not
+#include<stdio.h>
+int main(){
+
+
+int num; 
+printf("Enter the number\n");
+scanf("Divisiblity test returns; %d\n, num%97");
+
+// Q4. step by step evauluation of 3*x/y-z+k
+int x = 2, y = 3, z = 3, k = 1;
+int result = 3*x/y-z+k;
+// 6/3-3+1
+// 2-3+1
+// 0
+printf("the value of result is %d", result);
+
+return 0;
+}
